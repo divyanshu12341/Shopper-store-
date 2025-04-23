ccd7fb05028d4ff4df8eaec8b30c03da04b7762e
divyanshu12341 <divyanshujain111@gmail.com>
2025-04-23 22:57:37 +0530

    casing issue

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import "./ProductDisplay.css";
 import star_icon from '../assets/star_icon.png'
 import star_dull_icon from '../assets/star_dull_icon.png'
-import { ShopContext } from "../../Context/shopContext";
+import { ShopContext } from "../../Context/ShopContext";
 const ProductDisplay = (props) => {
   const { product } = props;
   const {addToCart} = useContext(ShopContext);
