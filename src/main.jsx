b7b2ffac595605fca8a40c297bc4a29eaeb3f209
divyanshu12341 <divyanshujain111@gmail.com>
2025-04-23 22:25:13 +0530

    fixed casing issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ShopContextProvider from './Context/shopContext'
+import ShopContextProvider from './Context/ShopContext'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ShopContextProvider>
