9c6321566846f7ec5dda540ca4353c859e6fae5e
divyanshu12341 <divyanshujain111@gmail.com>
2025-04-27 10:41:18 +0530

    Fixed dollar sign is not showing

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Item.css'
 const Item = (props) => {
   return (
     <div className="item">
@@ -9,10 +10,10 @@ const Item = (props) => {
         <p>{props.name}</p>
         <div className="item-prices">
             <div className="item-price-new">
-                {props.new_price}
+                ${props.new_price}
             </div>
             <div className="item-price-old">
-                {props.old_price}
+                ${props.old_price}
             </div>
         </div>
     </div>
